1b62097ea993606e4be1158971c34ba3f0ae85df
Bharath <nbharaths@gmail.com>
2019-02-27 23:39:55 -0500

    Class changes

--- src/main.py
+++ src/main.py
@@ -9,17 +9,20 @@ from node import Node
 
 def main():
     peer_type = ['buyer', 'seller']
-    df_ip = pd.read_csv('ips.csv')
+    df_ip = pd.read_csv('ips.csv', delimiter=',')
     N = df_ip.shape[0]
-    print('Number of nodes in network', N)
+    print('Number of nodes in network - ', N)
     df_conn = pd.read_csv('connections.csv')
 
     nodes = {}
     for index, row in df_ip.iterrows():
+        print(row['Node'], row['IP'])
         nodes[row['Node']] = Node(row['Node'], row['IP'], random.choice(peer_type))
 
     for index, row in df_conn.iterrows():
         nodes[row['Node1']].add_neighbour(row['Node2'])
+
+    print(nodes)
 
 
 if __name__ == "__main__":
