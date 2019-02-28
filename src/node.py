459c630e83274222ac44d9f0bcba270aeed2a498
ronakzala <ronakpzala@gmail.com>
2019-02-28 01:01:34 -0500

    Implemented the lookup() call.
    To do: Implement the list of peer id's in the lookup message

--- src/node.py
+++ src/node.py
@@ -6,11 +6,13 @@ import Pyro4
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
 class Node(object):
-    def __init__(self, id, ip, peertype):
+    def __init__(self, id, ip, peertype, product_name = None, product_count = None):
         self.id = id
         self.ip = ip
         self.peertype = peertype
         self.neighbourlist = []
+        self.product_name = product_name
+        self.product_count = product_count
 
     # Randomly assign buyer or seller
 
@@ -18,7 +20,17 @@ class Node(object):
         self.neighbourlist.append(neighbour)
 
     def lookup(self, product_name, hopcount):
-        pass
+
+        if hopcount == 0:
+            return
+
+        hopcount -= 1
+
+        if self.type == "seller" and product_name == self.product_name:
+            self.reply(self.id)
+
+        for neighbour in self.neighbourlist:
+            neighbour.lookup(product_name, hopcount)
 
     def reply(self, sellerid):
         pass
