3b9ca2e78bbdf2fe37d7005a0937a0c5ecfb5fb5
ronakzala <ronakpzala@gmail.com>
2019-03-02 01:29:31 -0500

    Implemented thread support and added node_start()

--- src/node.py
+++ src/node.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
 import random
-import socket
 
 import Pyro4
+import socket
+import threading as t
+import time
 
 products = ['fish', 'salt', 'boars']
 
@@ -22,6 +24,15 @@ class Node(object):
 
     # Randomly assign buyer or seller
 
+    def node_start(self):
+
+        start_time = time.localtime()
+
+        while time.time() < start_time + time.localtime(5):
+            time.sleep(0.25)
+            self.lookup(self.product_name, )
+
+
     def add_neighbour(self, neighbour):
         self.neighbourlist.append(neighbour)
 
@@ -32,7 +43,7 @@ class Node(object):
             self.product_name = random.choice(products)
             self.product_count = self.max_items
 
-    def lookup(self, product_name, hopcount, peer_path):
+    def lookup_t(self, product_name, hopcount, peer_path):
 
         if hopcount == 0:
             return
@@ -45,7 +56,11 @@ class Node(object):
         for neighbour in self.neighbourlist:
             neighbour.lookup(product_name, hopcount, peer_path + [self.id])
 
-    def reply(self, sellerid, peer_path):
+    def lookup(self, product_name, hopcount, peer_path):
+        x = t.Thread(target=self.lookup_t, args=(product_name, hopcount, peer_path,))
+        x.start()
+
+    def reply_t(self, sellerid, peer_path):
         if not peer_path:
             self.buy(sellerid)
 
@@ -54,16 +69,23 @@ class Node(object):
                 peer_path.pop()
                 neighbour.reply(sellerid, peer_path)
 
-    def buy(self, peerid):
+    def reply(self,  sellerid, peer_path):
+        x = t.Thread(target=self.reply_t, args=(sellerid, peer_path))
+        x.start()
+
+    def buy_t(self, peerid):
         peer_node = Pyro4.Proxy(peerid)
         peer_node.transact()
         print('Bought from ', peerid)
 
+    def buy(self,  peerid):
+        x = t.Thread(target=self.buy_t, args=(peerid))
+        x.start()
 
 def main():
     Pyro4.Daemon.serveSimple(
         {
-            Node: "src.node"
+            Node: "P1"
         },
         host=socket.gethostbyname(socket.gethostname()),
         ns=True)
