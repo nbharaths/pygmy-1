bc99fa1fad8d074c804b898913f71f3234119026
Bharath <nbharaths@gmail.com>
2019-03-05 02:49:56 -0500

    In-line comments for node.py

--- src/node.py
+++ src/node.py
@@ -4,13 +4,16 @@ import random
 import sys
 import threading as t
 import time
-import socket
 
 import Pyro4
 
 products = ['fish', 'salt', 'boars']
-Pyro4.config.NS_HOST = '128.119.243.168' #  socket.gethostbyname(socket.gethostname())
-Pyro4.config.NS_PORT = 8111
+BUYER = 'buyer'  # Constants for readability
+SELLER = 'seller'
+
+
+# Pyro4.config.NS_HOST = '192.168.43.23' #  socket.gethostbyname(socket.gethostname())
+# Pyro4.config.NS_PORT = 9090
 
 
 @Pyro4.expose
@@ -28,9 +31,10 @@ class Node(object):
     wait_time = None
     can_buy = True
     lock = t.Lock()
+    product_to_buy = None
 
     # exposed
-    def get_peertype(self):
+    def get_peertype(self):  # Methods exposed for remote calls
         return self.peertype
 
     # exposed
@@ -45,18 +49,17 @@ class Node(object):
     def get_product_to_buy(self):
         return self.product_to_buy
 
-    def init(self, node_id, ip, peertype, wait_time = 4, product_count = 2, hop_count = 3):
-        self.node_id = node_id
-        self.ip = ip
-        self.peertype = peertype
-        self.neighbourlist = []
-        self.wait_time = wait_time
-        if peertype == 'seller':
+    def init(self, node_id, ip, peertype, wait_time=4, product_count=2, hop_count=3):
+        self.node_id = node_id  # Id of the node
+        self.ip = ip  # IP address of the node
+        self.peertype = peertype  # Type of the peer (buyer/seller)
+        self.neighbourlist = []  # List of adjacent peers
+        self.wait_time = wait_time  # Time to wait after buyer lookup
+        if peertype == SELLER:
             self.product_name = random.choice(products)
             self.product_count = product_count
         else:
-            self.replies = []
-            self.hop_count = hop_count
+            self.hop_count = hop_count  # Hop counts for lookup
         print("Initialized ", self.peertype, " ID -", self.node_id)
 
     def node_start_t(self):
@@ -64,12 +67,12 @@ class Node(object):
             wait_time = random.random() * self.wait_time  # Multiplying with 1000 for milliseconds
 
             self.product_to_buy = random.choice(products)
-            print("!! Searching for", self.product_to_buy)
-            self.lookup(self.product_to_buy, self.hop_count, [])
+            print("Searching for", self.product_to_buy)
+            self.lookup(self.product_to_buy, self.hop_count, [])  # Start the lookup call
             time.sleep(wait_time)
 
-            if self.sellers_list:
-                selected_seller = random.choice(self.sellers_list)
+            if self.sellers_list:  # If list of sellers populated
+                selected_seller = random.choice(self.sellers_list)  # Choose a seller randomly and try to transact
                 self.buy(selected_seller)
             else:
                 print("Buy order failed, wait time was set to ", wait_time)
@@ -83,18 +86,18 @@ class Node(object):
         return
 
     def add_neighbour(self, neighbour):
-        self.neighbourlist.append(neighbour)
+        self.neighbourlist.append(neighbour)  # Add adjacent peer to current peer's list
 
     def transact(self, buyerid):
-        if self.product_count < 1:
+        if self.product_count < 1:  # Ensures positive product counts
             self.product_name = random.choice(products)
             self.product_count = 3
-            print("No products left!! Re-initializing with", self.product_name, self.product_count)
+            print("No products left! Re-initializing with", self.product_name, self.product_count)
             return None
 
         else:
             buyer_node = Pyro4.Proxy("PYRONAME:" + buyerid)
-            if self.product_name == buyer_node.get_product_to_buy():  # This ensure that you are buying what you want
+            if self.product_name == buyer_node.get_product_to_buy():  # This ensures that you are buying what you want
                 self.product_count -= 1
                 print("Selling", self.product_name, "Product Count", self.product_count)
                 return self.node_id, self.product_name
@@ -104,13 +107,13 @@ class Node(object):
         if hopcount == 0:
             return
 
-        hopcount -= 1
+        hopcount -= 1  # Decrement hopcount
 
-        if self.peertype == "seller" and product_name == self.product_name:
-            print("Sending prompt to node -", peer_path[0])
+        if self.peertype == SELLER and product_name == self.product_name:
+            print("Sending prompt to node -", peer_path[0])  # Send a reply
             self.reply(self.node_id, peer_path)
 
-        for neighbour in self.neighbourlist:
+        for neighbour in self.neighbourlist:  # Call lookup in a flood-fill manner
             neighbour.lookup(product_name, hopcount, peer_path + [self.node_id])
 
     def lookup(self, product_name, hopcount, peer_path):
@@ -118,14 +121,14 @@ class Node(object):
         lookup_thread.start()
 
     def reply_t(self, sellerid, peer_path):
-        if len(peer_path) < 1:
+        if len(peer_path) < 1:  # Reply has come back to original buyer
             peer_node = Pyro4.Proxy("PYRONAME:" + sellerid)
-            if self.product_to_buy == peer_node.get_product_name():  # This ensure that you are buying what you want
+            if self.product_to_buy == peer_node.get_product_name():  # This ensures that you are buying what you want
                 self.sellers_list.append(sellerid)
                 return
 
         for neighbour in self.neighbourlist:
-            if peer_path and neighbour.get_node_id() == peer_path[-1]:
+            if peer_path and neighbour.get_node_id() == peer_path[-1]:  # Reverse the path to buyer
                 peer_path.pop()
                 neighbour.reply(sellerid, peer_path)
 
@@ -137,20 +140,23 @@ class Node(object):
         peer_node = Pyro4.Proxy("PYRONAME:" + peerid)
         # print("Buying from", peerid, peer_node.get_product_name())
         # print("Trying to acquire")
-        self.lock.acquire()
+        self.lock.acquire()  # Acquire a lock to ensure proper output if concurrent requests
         # print("Acquired the lock")
         seller_id = peer_node.transact(self.node_id)
         # print("Trying to release")
-        self.lock.release()
+        self.lock.release()  # Release lock after transaction
         if seller_id:
             print("Bought from", seller_id)
         else:
             print("Couldn't buy")
         # print("Released the lock")
 
+
 def main():
     node_id = sys.argv[1]
-    Pyro4.Daemon.serveSimple({Node: node_id}, host=socket.gethostbyname(socket.gethostname()), ns=True)
+    # print(socket.gethostbyname(socket.gethostname()))
+    Pyro4.Daemon.serveSimple({Node: node_id}, host='192.168.0.6', ns=True)  # Starts the Server
+
 
 if __name__ == "__main__":
     main()
