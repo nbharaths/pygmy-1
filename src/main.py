210f912003a5879fdbf787c1a861d42fb44c4c57
ronakzala <ronakpzala@gmail.com>
2019-03-05 23:36:49 -0500

    Added inline comments for good readability

--- src/main.py
+++ src/main.py
@@ -5,25 +5,37 @@ import random
 import Pyro4
 import pandas as pd
 
+# List of products that will be part of the bazaar
 products = ['fish', 'salt', 'boars']
-BUYER = 'buyer'  # Constants for readability
+
+# Constants for readability
+BUYER = 'buyer'
 SELLER = 'seller'
 
 
 def main():
+
+    # Currently two possible peer types
     peer_types = ['buyer', 'seller']
+
+    # Reading the csv file for getting the IP's corresponding to the nodes
     df_ip = pd.read_csv('ips.csv', delimiter=',')  # Read in Node IDs as well as IP Addresses
     df_params = pd.read_csv('params.csv', delimiter=',')
     max_items = int(df_params['Value'][0])
     hop_count = int(df_params['Value'][1])
     max_wait_time = int(df_params['Value'][2])
+
+    # Setting the Pyro nameserver host and port
     NS_HOST = df_params['Value'][3]
     NS_PORT = int(df_params['Value'][4])
     n = df_ip.shape[0]
+
     print('Number of nodes in the network - ', n)
     df_conn = pd.read_csv('connections.csv', delimiter=',')  # Read in the network topology edge-wise
 
     nodes = {}  # Dictionary that holds the Pyro4 proxy objects
+
+    # Initializing the the nodes
     for index, row in df_ip.iterrows():
         print("Connecting to URI for :", row['Node'])
         ns = Pyro4.locateNS(host=NS_HOST, port=NS_PORT)  # use your own nameserver
@@ -34,10 +46,12 @@ def main():
                                 NS_PORT)  # Instantiating the peer with buyer/seller randomly
         print(nodes[row['Node']].get_peertype())
 
-    for index, row in df_conn.iterrows():  # Adding adjacent peers for each peer to facilitate lookup
+    # Adding adjacent peers for each peer to facilitate lookup
+    for index, row in df_conn.iterrows():
         nodes[row['Node1']].add_neighbour(nodes[row['Node2']])
         nodes[row['Node2']].add_neighbour(nodes[row['Node1']])
 
+    # Starting the bazaar
     for node in nodes.values():
         print("Starting node -", node.get_node_id())
         if node.get_peertype() == BUYER:  # Starts the lookup calls for every buyer
--- src/node.py
+++ src/node.py
@@ -9,14 +9,19 @@ import time
 import Pyro4
 import pandas as pd
 
+# List of products that will be part of the bazaar
 products = ['fish', 'salt', 'boars']
-BUYER = 'buyer'  # Constants for readability
+
+# Constants for readability
+BUYER = 'buyer'
 SELLER = 'seller'
 
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
 class Node(object):
+
+    # Object variable initialized, not keeping them in an init function call because of a Pyro constraint
     node_id = None
     ip = None
     peertype = None
@@ -33,8 +38,9 @@ class Node(object):
     product_to_buy = None
     timestamp = None
 
+    # Methods exposed for making the Pyro proxy objects get the attributes of actual object
     # exposed
-    def get_peertype(self):  # Methods exposed for remote calls
+    def get_peertype(self):
         return self.peertype
 
     # exposed
@@ -49,11 +55,12 @@ class Node(object):
     def get_product_to_buy(self):
         return self.product_to_buy
 
+    # Custom init call kept due to Pyro proxy object initialization constraints
     def init(self, node_id, ip, peertype, wait_time=4, product_count=2, hop_count=3, ns_host=None, ns_port=None):
-        self.node_id = node_id  # Id of the node
-        self.ip = ip  # IP address of the node
-        self.peertype = peertype  # Type of the peer (buyer/seller)
-        self.neighbourlist = []  # List of adjacent peers
+        self.node_id = node_id      # Id of the node
+        self.ip = ip                # IP address of the node
+        self.peertype = peertype    # Type of the peer (buyer/seller)
+        self.neighbourlist = []     # List of adjacent peers
         self.wait_time = wait_time  # Time to wait after buyer lookup
         if peertype == SELLER:
             self.product_name = random.choice(products)
@@ -66,7 +73,7 @@ class Node(object):
 
     # Thread for starting the lookup call along with the background timer until the wait time
     def node_start_t(self):
-        for i in range(100):  # TO DO: Needs to be run infinitely
+        for i in range(100):  # Currently kept it 100 not letting the network run for infinite time
 
             # Reinitializing the lists
             self.sellers_list = []
@@ -89,6 +96,7 @@ class Node(object):
             else:
                 print("Buy order failed, wait time was set to ", wait_time)
 
+    # This is the method starting the node thread
     def node_start(self):
         print("Creating the node thread", self.node_id)
         node_start_thread = t.Thread(target=self.node_start_t)
@@ -97,9 +105,11 @@ class Node(object):
         print("Started the node thread", self.node_id)
         return
 
+    # Method for adding the neighbouring peer node in the topology into the current node's list
     def add_neighbour(self, neighbour):
         self.neighbourlist.append(neighbour)  # Add adjacent peer to current peer's list
 
+    # Final method called from the buy method to complete the transaction
     def transact(self, buyerid):
         if self.product_count < 1:  # Ensures positive product counts
             self.product_name = random.choice(products)
@@ -116,6 +126,7 @@ class Node(object):
                 print("Selling", self.product_name, "Product Count", self.product_count)
                 return self.node_id, self.product_name
 
+    # The lookup thread starter method
     def lookup_t(self, product_name, hopcount, peer_path, timestamp):
 
         if hopcount == 0:
@@ -130,10 +141,12 @@ class Node(object):
         for neighbour in self.neighbourlist:  # Call lookup in a flood-fill manner
             neighbour.lookup(product_name, hopcount, peer_path + [self.node_id], timestamp)
 
+    # The lookup thread wrapper method
     def lookup(self, product_name, hopcount, peer_path, timestamp):
         lookup_thread = t.Thread(target=self.lookup_t, args=(product_name, hopcount, peer_path, timestamp))
         lookup_thread.start()
 
+    # The reply thread starter method
     def reply_t(self, sellerid, peer_path, timestamp):
         if len(peer_path) < 1:  # Reply has come back to original buyer
             ns = Pyro4.locateNS(host=self.ns_host, port=self.ns_port)  # use your own nameserver
@@ -149,10 +162,12 @@ class Node(object):
                 peer_path.pop()
                 neighbour.reply(sellerid, peer_path, timestamp)
 
+    # The reply thread wrapper method
     def reply(self, sellerid, peer_path, timestamp):
         reply_thread = t.Thread(target=self.reply_t, args=(sellerid, peer_path, timestamp))
         reply_thread.start()
 
+    # Buy method that will be called by the buyer nodes
     def buy(self, peerid):
         ns = Pyro4.locateNS(host=self.ns_host, port=self.ns_port)  # use your own nameserver
         uri = ns.lookup(peerid)
@@ -178,11 +193,13 @@ class Node(object):
 
 
 def main():
+    # Reading from the csv file
     node_id = sys.argv[1]
     df_params = pd.read_csv('params.csv', delimiter=',')
     NS_HOST = df_params['Value'][3]
     NS_PORT = int(df_params['Value'][4])
 
+    # Creating the Pyro Daemon
     daemon = Pyro4.Daemon(host=socket.gethostbyname(socket.gethostname()))
     ns = Pyro4.locateNS(host=NS_HOST, port=NS_PORT)
     uri = daemon.register(Node)
